ccee90cf4fe2888e4f350d8231a90fe1755db5e9
bibin k b <bibinkochumalayil007@gmail.com>
2023-10-21 23:43:54 +0530

    Bloc added

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:student_records/model/student_model.dart';
-import 'package:student_records/provider/provider.dart';
 import 'package:student_records/view/home_page/homepage.dart';
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   if (!Hive.isAdapterRegistered(StudentModelAdapter().typeId)) {
     Hive.registerAdapter(StudentModelAdapter());
   }
-  runApp(ChangeNotifierProvider(
-    create: (context) => ProviderClass(),
-    child: MyApp(),
-  ));
+  runApp(
+    const MyApp(),
+  );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
--- lib/provider/provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:student_records/model/student_model.dart';
-
-class ProviderClass with ChangeNotifier {
-  List<StudentModel> studentList = [];
-  List<StudentModel> studentSearchResult = [];
-}
